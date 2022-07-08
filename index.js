62eeeeb878b23536cfb64c99b6e1beec69bb52a5
vincadrn <wsamudro@gmail.com>
2022-07-08 12:34:38 +0700

    Modify to make the app functions

--- index.js
+++ index.js
@@ -24,7 +24,15 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
-
+app.get("/api/:date", function (req, res) {
+  var date = new Date(Number(req.params.date));
+  if (date.toString() === "Invalid Date") {
+    res.json({error: date.toString()});
+  }
+  else {
+    res.json({unix: Number(req.params.date), utc: date.toGMTString()});
+  }
+});
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
