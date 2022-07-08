0a0a003397e74cf5df0adaed2de17562862c4d33
vincadrn <wsamudro@gmail.com>
2022-07-08 13:18:30 +0700

    Modify index.js to add flexibility to the input date

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var express = require('express');
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
+// so that the API is remotely testable by FCC 
 var cors = require('cors');
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
@@ -18,19 +18,30 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+let date;
 
-// your first API endpoint... 
+app.get("/api", function (req, res) {
+  date = new Date(Date.now());
+  res.json({unix: date.getTime(), utc: date.toGMTString()});
+})
+
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
 app.get("/api/:date", function (req, res) {
-  var date = new Date(Number(req.params.date));
-  if (date.toString() === "Invalid Date") {
+  if (isNaN(Number(req.params.date))) { // check whether the date is in string format
+    date = new Date(req.params.date);
+  }
+  else { // the date is in integer/milliseconds format
+    date = new Date(Number(req.params.date));
+  }
+  
+  if (date.toString() === "Invalid Date") { // check whether date is invalid after parsing
     res.json({error: date.toString()});
   }
-  else {
-    res.json({unix: Number(req.params.date), utc: date.toGMTString()});
+  else { // the date is valid
+    res.json({unix: date.getTime(), utc:date.toGMTString()});
   }
 });
 
